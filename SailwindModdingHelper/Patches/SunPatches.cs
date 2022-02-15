9ac0efc60a887b3dc56ef90d3f721f24702cf1da
Ricardo <rmmx03@gmail.com>
2022-02-15 00:42:56 +0000

    Added player position and controller

---
+++ SailwindModdingHelper/Patches/SunPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SailwindModdingHelper.Patches
+{
+    public static class SunPatches
+    {
+        [HarmonyPatch(typeof(Sun), "Start")]
+        public static class StartPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                PlayerClimb component = GameObject.FindObjectOfType<PlayerClimb>();
+                Utils.SetPlayerController((CharacterController)Traverse.Create(component).Field("controller").GetValue());
+            }
+        }
+    }
+}
---
+++ SailwindModdingHelper/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SailwindModdingHelper
+{
+    public static class Utils
+    {
+        static CharacterController characterController;
+        static Transform playerTransform;
+        public static CharacterController CharacterController => characterController;
+        public static Transform PlayerTransform => playerTransform;
+
+        internal static void SetPlayerController(CharacterController characterController)
+        {
+            Utils.characterController = characterController;
+            playerTransform = characterController.transform;
+        }
+
+    }
+}
