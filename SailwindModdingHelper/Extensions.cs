7967961f6720159510bbd874b57c28228f7b764a
Ricardo <rmmx03@gmail.com>
2022-03-07 09:58:42 +0000

    more extensions

--- SailwindModdingHelper/Extensions.cs
+++ SailwindModdingHelper/Extensions.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SailwindModdingHelper
 {
@@ -32,6 +34,37 @@ namespace SailwindModdingHelper
         public static T InvokePrivateMethod<T>(this object obj, string method, params object[] parameters)
         {
             return (T)obj.InvokePrivateMethod(method, parameters);
+        }
+
+        // https://answers.unity.com/questions/530178/how-to-get-a-component-from-an-object-and-add-it-t.html
+        public static T GetCopyOf<T>(this Component comp, T other) where T : Component
+        {
+            Type type = comp.GetType();
+            if (type != other.GetType()) return null; // type mis-match
+            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+            PropertyInfo[] pinfos = type.GetProperties(flags);
+            foreach (var pinfo in pinfos)
+            {
+                if (pinfo.CanWrite)
+                {
+                    try
+                    {
+                        pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+                    }
+                    catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+                }
+            }
+            FieldInfo[] finfos = type.GetFields(flags);
+            foreach (var finfo in finfos)
+            {
+                finfo.SetValue(comp, finfo.GetValue(other));
+            }
+            return comp as T;
+        }
+
+        public static T AddComponent<T>(this GameObject go, T toAdd) where T : Component
+        {
+            return go.AddComponent<T>().GetCopyOf(toAdd) as T;
         }
     }
 }
